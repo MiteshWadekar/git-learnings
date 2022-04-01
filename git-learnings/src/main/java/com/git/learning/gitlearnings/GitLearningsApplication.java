de68b62de3155c422194c07dc3d97cd02f44bdba
Mitesh Wadekar <wadekarmitesh@gmail.com>
2022-04-01 23:16:29 +0530

    Initial Commit

---
+++ git-learnings/src/main/java/com/git/learning/gitlearnings/GitLearningsApplication.java
@@ -0,0 +1,13 @@
+package com.git.learning.gitlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLearningsApplication.class, args);
+	}
+
+}
---
+++ git-learnings/src/test/java/com/git/learning/gitlearnings/GitLearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.learning.gitlearnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
